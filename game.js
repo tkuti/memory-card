7896bf92122e9c2d3e544c69db85b389687d0891
tkuti <tunde.kuti@gmail.com>
2021-11-18 17:51:18 +0100

    images.js, createBoard

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-console.log("HI")
+const _board = document.querySelector(".board")
+
+createCards()
+
+const _cards = document.querySelectorAll('.card')
+
+
+function createCards () {
+    imagesOfDogs.forEach(img => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        const frontFace = document.createElement('img')
+        frontFace.setAttribute('src', img.src)
+        frontFace.setAttribute('alt', img.name)
+        frontFace.classList.add('front-face')
+        const backFace = document.createElement('img')
+        backFace.setAttribute('src', './images/cover.jpg')
+        backFace.setAttribute('alt', 'cover')
+        backFace.classList.add('back-face')
+        _board.appendChild(card)
+        card.appendChild(frontFace)
+        card.appendChild(backFace)
+    })
+}
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+_cards.forEach(card => card.addEventListener('click', flipCard));
---
+++ images.js
@@ -0,0 +1,50 @@
+const imagesOfDogs = [
+    {
+        name: "amstaff",
+        src: "./images/dogs/amstaff.jpg"
+    },
+    {
+        name: "amstaff",
+        src: "./images/dogs/amstaff.jpg"
+    },
+    {
+        name: "australian",
+        src: "./images/dogs/australian.jpg"
+    },
+    {
+        name: "australian",
+        src: "./images/dogs/australian.jpg"
+    },
+    {
+        name: "beagle",
+        src: "./images/dogs/beagle.jpg"
+    },
+    {
+        name: "beagle",
+        src: "./images/dogs/beagle.jpg"
+    },
+    {
+        name: "bernese",
+        src: "./images/dogs/bernese.jpg"
+    },
+    {
+        name: "bernese",
+        src: "./images/dogs/bernese.jpg"
+    },
+    {
+        name: "boxer",
+        src: "./images/dogs/boxer.jpg"
+    },
+    {
+        name: "boxer",
+        src: "./images/dogs/boxer.jpg"
+    },
+    {
+        name: "cairn",
+        src: "./images/dogs/cairn.jpg"
+    },
+    {
+        name: "cairn",
+        src: "./images/dogs/cairn.jpg"
+    }
+]
