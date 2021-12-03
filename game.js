22e711f717c1550f6e49dfbe67c4ee115ff40d81
tkuti <tunde.kuti@gmail.com>
2021-12-03 14:34:44 +0100

    results - store + generate table

--- game.js
+++ game.js
@@ -10,16 +10,22 @@ const _remainingPairs = document.querySelector("#remaining-pairs")
 const _time = document.querySelector("#time")
 const _gameOverContainer = document.querySelector('#game-over-container')
 const _newGameBtn = document.querySelector('#new-game-btn')
+const _resultsBtns = document.querySelectorAll('.results-btn')
+const _resultsCloseBtns = document.querySelectorAll('.results-close-btn')
+const _resultsContainer = document.querySelector('.results-container')
+const _resultTypeBtns = document.querySelectorAll('.result-type-btn')
 
 let levelClass = 'demo'
 let screenType = window.innerWidth > window.innerHeight ? 'landscape' : 'portrait'
 let numberOfFlippedCards
 let numberOfRemainingPairs
 let timer
+let finalTime
 let images
 let hasFlippedCard = false
 let lockBoard = false
 let firstCard, secondCard
+let results
 
 _startBtn.addEventListener('click', () => {
     _welcomeContainer.style.display = 'none'
@@ -42,6 +48,36 @@ window.addEventListener('resize', () => {
     window.innerWidth > window.innerHeight ? screenType = 'landscape' : screenType = 'portrait'
     setLevel(levelClass)
 }) 
+
+_resultsBtns.forEach(btn => btn.addEventListener('click', () => {
+    _resultsContainer.style.display = 'flex'
+    _resultTypeBtns.forEach(bt => {
+        if (bt.innerHTML !== "Demo") {
+            bt.classList.remove('active')
+        } else {
+            bt.classList.add("active")
+        }
+    })
+    generateResultTable('demo')
+}))
+
+
+_resultTypeBtns.forEach(btn => btn.addEventListener('click', (e) => {
+    _resultTypeBtns.forEach(bt => {
+        bt.classList.remove('active')
+    })
+    e.target.classList.add('active')
+    generateResultTable(e.target.innerHTML.toLowerCase())
+}))
+
+_resultsCloseBtns.forEach(btn => btn.addEventListener('click', () => {
+    _resultsContainer.style.display = 'none'
+}))
+
+
+const savedResults = localStorage.getItem('results') 
+savedResults ? results = JSON.parse(savedResults) : results = []
+
 
 function setLevel(level) {
     resetGame()
@@ -100,15 +136,12 @@ function flipCard() {
     numberOfFlippedCards++
     _flippedCards.innerHTML = numberOfFlippedCards
     numberOfFlippedCards === 1 && startTime()
-
     if (!hasFlippedCard) {
         hasFlippedCard = true
         firstCard = this
         return
     }
-
     secondCard = this
-
     firstCard.dataset.name === secondCard.dataset.name
         ? disableCards()
         : unflipCards()
@@ -148,7 +181,7 @@ function resetGame() {
 
 function startTime() {
     let seconds = 0
-    let minutes = 0
+    let minutes = '00'
     timer = setInterval(() => {
         seconds++
         if (seconds < 10) {
@@ -157,6 +190,9 @@ function startTime() {
         if (seconds > 59) {
             minutes++
             seconds = "00"
+            if (minutes < 10) {
+                minutes = "0" + minutes
+            }
         }
         let clockFace = minutes + ":" + seconds
         _time.innerHTML = clockFace
@@ -165,20 +201,72 @@ function startTime() {
 
 function stopTime() {
     clearInterval(timer)
-    _time.innerHTML = '0:00'
+    finalTime = _time.innerHTML
+    _time.innerHTML = '00:00'
 }
 
 function gameOver() {
+    stopTime()
     let _resultLevel = document.querySelector("#result-level")
     let _resultTime = document.querySelector("#result-time")
     let _resultFlips = document.querySelector("#result-flips")
     let _resultName = document.querySelector("#result-name")
     _gameOverContainer.style.display = "flex"
-    _resultTime.innerHTML = _time.innerHTML
+    _resultTime.innerHTML = finalTime
     _resultFlips.innerHTML = _flippedCards.innerHTML
-    username != ""
-        ? _resultName.innerHTML = username
-        : _resultName.innerHTML = "Stranger"
+    if ( !username ) username = "Stranger"
+    _resultName.innerHTML = username
     _resultLevel.innerHTML = _levelSelect.value
-    stopTime();
+    saveResult()
+}
+
+function saveResult () {
+    const newResult = {
+        name: username,
+        time: finalTime,
+        flips: _flippedCards.innerHTML,
+        level: _levelSelect.value
+    }
+    results.push(newResult)
+    localStorage.setItem('results', JSON.stringify(results))
+}
+
+function sortResults (filteredResults) {
+    filteredResults.sort((a, b) => {
+        if (a.time < b.time) {
+            return -1
+        } else if (a.time > b.time) {
+            return 1
+        } else {
+            return a.flips - b.flips
+        }
+    })
+}
+
+function generateResultTable (level) {
+    const filteredResults = results.filter(res => res.level === level)
+    sortResults(filteredResults)
+    const _resultTableBody = document.querySelector('tbody')
+    _resultTableBody.innerHTML = ""
+    if (filteredResults.length > 0) {
+        filteredResults.forEach((result, index) => {
+            const row = document.createElement('tr')
+            const nrTd = document.createElement('td')
+            nrTd.innerHTML = index + 1
+            const nameTd = document.createElement('td')
+            nameTd.innerHTML = result.name
+            const timeTd = document.createElement('td')
+            timeTd.innerHTML = result.time
+            const flipsTd = document.createElement('td')
+            flipsTd.innerHTML = result.flips
+            const levelTd = document.createElement('td')
+            levelTd.innerHTML = result.level
+            row.appendChild(nrTd)
+            row.appendChild(nameTd)
+            row.appendChild(timeTd)
+            row.appendChild(flipsTd)
+            row.appendChild(levelTd)
+            _resultTableBody.appendChild(row)
+        })
+    }
 }
